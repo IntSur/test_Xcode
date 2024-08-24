681005637f93c5ac376689ee4509aef0cffde19c
fanjiahui <1203394464@qq.com>
2024-08-24 16:15:31 +0800

    Initial Commit

---
+++ MoonShoot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoonShoot
+//
+//  Created by 樊嘉辉 on 2024/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoonShoot/MoonShootApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShootApp.swift
+//  MoonShoot
+//
+//  Created by 樊嘉辉 on 2024/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
